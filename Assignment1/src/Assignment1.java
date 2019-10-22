68c73370edcab56ea070deac1c35fe102eba0a14
mialdz9889 <milad.zolfaghari98@gmail.com>
2019-10-21 22:39:35 -0700

    Pretty much done unless I want to enhance

--- Assignment1/src/Assignment1.java
+++ Assignment1/src/Assignment1.java
@@ -17,60 +17,68 @@ public class Assignment1 {
 
 		while (userInput.hasNextDouble()) {
 			System.out.println("ERROR: You aren't a robot. Use the name your parents gave you using only letters :)");
-			userInput.next();
+			userInput.nextLine();
 		}
 
-		String name = userInput.next();
-	
+		String name = userInput.nextLine();
+
 		// Print out name and then ask for user to input a height. Validate that the
 		// user inputs a double and that the number is between 0 and 1000
 
-		System.out.println("Hello, " + name + "." + " Please enter the height of a right triangle");
+		System.out.println("Hello, " + name);
+		System.out.println("Please enter the height of a right triangle");
 
-		Double triangleHeight = -1.0;
+		Double triangleHeight = 0.0;
 
-		while (triangleHeight > 1000 || triangleHeight < 0) {
-			System.out.println("Invalid number. It must be between 0 and 1000.");
-
+		while (triangleHeight < 1 || triangleHeight > 1000) {
+			System.out.println("Please enter a number between 0 and 1000");
 			while (!userInput.hasNextDouble()) {
-
+				System.out.println("Needs to be a number!");
 				userInput.next();
+
 			}
+			;
+
 			triangleHeight = userInput.nextDouble();
 		}
 
 		System.out.println("Now enter the base of the same right triangle: ");
 
-		while (!userInput.hasNextDouble())
+		Double triangleBase = 0.0;
 
-		{
-			System.out.println("ERROR: Please enter a number!");
-			userInput.nextLine();
-			System.exit(1);
+		while (triangleBase < 1 || triangleBase > 1000) {
+			System.out.println("Please enter a number between 0 and 1000");
+			while (!userInput.hasNextDouble()) {
+				System.out.println("Needs to be a number!");
+				userInput.next();
+
+			}
+			;
+
+			triangleBase = userInput.nextDouble();
 		}
 
-		double base = 0;
-		if (base <= 0 || base > 1000) {
-			System.out.println("ERROR: Number must be between 0 and 1000");
-
-		}
-
-		Double triangleBase = userInput.nextDouble();
-
-		double triangleArea = (triangleHeight * triangleBase) / 2;
+		Double triangleArea = (triangleHeight * triangleBase) / 2;
 
 		System.out.println("The area of this triangle is : " + triangleArea);
 
 		System.out.println("Now enter the radius of a circle: ");
-		if (!userInput.hasNextDouble()) {
-			System.out.println("ERROR: Please enter a number!");
-			System.exit(1);
-			userInput.hasNextLine();
 
+		Double circleRadius = 0.0;
+
+		while (circleRadius < 1 || circleRadius > 1000) {
+			System.out.println("Please enter a number between 0 and 1000");
+			while (!userInput.hasNextDouble()) {
+				System.out.println("Needs to be a number!");
+				userInput.next();
+
+			}
+			
+
+			circleRadius = userInput.nextDouble();
 		}
-		;
 
-		double circleRadius = userInput.nextDouble();
+		
 
 		double circleArea = Math.PI * circleRadius * circleRadius;
 		double circleParimeter = 2 * Math.PI * circleRadius;
@@ -85,11 +93,10 @@ public class Assignment1 {
 			System.out.println("That is correct!");
 		} else {
 			System.out.println("That is not correct!");
-			System.exit(1);
+			
 		}
 
-		System.out.println(
-				"=============================================================================================================================");
+		System.out.println("=============================================================================================================================");
 	}
 
 }
